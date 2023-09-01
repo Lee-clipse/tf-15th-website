f19d33773c53fe9823d8c2bf1a5315df828d9b7f
edac99 <79911816+edac99@users.noreply.github.com>
2023-09-01 19:03:40 +0900

    feat: Set env for DB Connection

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,13 +1,14 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UserEntity } from 'src/domain/user/entity/user.entity';
+import { ENV } from '../constants/env';
 
 export const typeormConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '111111',
-  database: 'tf',
+  host: ENV.HOST,
+  port: ENV.PORT,
+  username: ENV.USERNAME,
+  password: ENV.PASSWORD,
+  database: ENV.DATABASE,
   entities: [UserEntity],
   synchronize: true,
 };
---
+++ src/constants/env.ts
@@ -0,0 +1,7 @@
+export const ENV = {
+  HOST: 'db-ie0tg-kr.vpc-pub-cdb.ntruss.com',
+  PORT: 3306,
+  USERNAME: 'tftf',
+  PASSWORD: '2023busan-tf',
+  DATABASE: 'tf',
+};
