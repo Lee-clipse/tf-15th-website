c92e894a02ec86e4baeb75c77d2567ed67e4860f
edac99 <79911816+edac99@users.noreply.github.com>
2023-09-09 13:37:54 +0900

    feat: Set API url by API rule book
    Add Game Server API too
    By API rule book at my notion

--- client/src/constants/env.js
+++ client/src/constants/env.js
@@ -3,10 +3,20 @@ export const ENV = {
   SERVER_DEV_DOMAIN: "http://localhost:4000/api",
   CLIENT_PROD_DOMAIN: "https://busan-tf.n-e.kr",
   SERVER_PROD_DOMAIN: "/api",
+  GAME_SERVER_DOMAIN: "/game-api",
 };
 
 export const API = {
   USER_REGISTER: "/user/register",
   RECONFIRM_QR: "/user/reconfirm-qr",
   USER_INFO: "/user/info",
+  GET_USER_TEAM: "/user/team",
+  JOIN_USER: "/user/join",
+  CREATE_TEAM: "/team/create",
+  VIEW_WAITING_TEAM: "/team/waiting",
+  VIEW_TEAM_SCORE: "/team/score",
+  PLUS_TEAM_SCORE: "/team/plus",
+  INIT_TEAM_MAP_INDEX: "/init",
+  VIEW_MAP_INDEX: "/where",
+  ROLL_DICE: "/next",
 };
