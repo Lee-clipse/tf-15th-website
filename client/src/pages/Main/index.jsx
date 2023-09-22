693ad5d00166a67f09908b83a5f371eaef97f9a3
edac99 <79911816+edac99@users.noreply.github.com>
2023-09-23 01:38:08 +0900

    feat: Implement D-Day coming map at Main

--- client/src/pages/Main/index.jsx
+++ client/src/pages/Main/index.jsx
@@ -5,9 +5,9 @@ import MainLinker from "@components/MainLinker";
 import YoutubeSection from "@components/YoutubeSection";
 import RegisterButton from "@components/RegisterButton";
 import ScrollInducer from "@components/ScrollInducer";
-import CountDown from "@components/CountDown";
 import moment from "moment";
 import { MainPopup } from "@components/MainPopup";
+import ComingMap from "../../component/ComingMap";
 
 const MainPage = () => {
   const [showMainPop, setShowMainPop] = useState(false);
@@ -36,35 +36,13 @@ const MainPage = () => {
     <PageTemplate>
       {showMainPop && <MainPopup setShowMainPop={setShowMainPop} />}
       <MainLogo />
-      <CountDown timeInfo={getDdayTime()} />
+      <ComingMap />
       <MainLinker />
       <YoutubeSection />
       <RegisterButton />
       <ScrollInducer />
     </PageTemplate>
   );
-};
-
-const getDdayTime = () => {
-  const targetDate = moment("2023-09-23 00:00:00");
-
-  const now = moment();
-  const distance = targetDate - now;
-
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-
-  const hoursValue = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const hoursString = String(hoursValue).padStart(2, "0");
-
-  const minutesValue = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const minutesString = String(minutesValue).padStart(2, "0");
-
-  const secondsValue = Math.floor((distance % (1000 * 60)) / 1000);
-  const secondsString = String(secondsValue).padStart(2, "0");
-
-  const countdownDayText = `D - ${days}`;
-  const countdownText = `${hoursString}:${minutesString}:${secondsString}`;
-  return { countdownDayText, countdownText };
 };
 
 export default MainPage;
