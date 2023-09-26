41179d0c6c37c26936e696768085862d2fdf08ff
edac99 <jamjam1208@naver.com>
2023-09-26 15:02:28 +0900

    modify: Block server access
    Block user register, reconfirm QR, enter zerogame for drop DB server

--- client/src/pages/Mission/index.jsx
+++ client/src/pages/Mission/index.jsx
@@ -3,17 +3,11 @@ import React, { useEffect } from "react";
 import * as s from "./style";
 import PageTemplate from "../PageTemplate";
 import TopNavBar from "@common/layer/TopNavBar";
-import { useNavigate } from "react-router-dom";
-import { RoutePath } from "@constants/enums";
 import Swal from "sweetalert2";
-import axios from "axios";
-import { ENV, API } from "@constants/env";
 import { motion } from "framer-motion";
 import { OchestraList } from "@styles/animation";
 
 const MissionPage = () => {
-  const navigate = useNavigate();
-
   useEffect(() => {
     window.scrollTo(0, 0);
 
@@ -34,29 +28,33 @@ const MissionPage = () => {
     }
   };
 
-  const routeToZeroGame = async () => {
-    const userId = localStorage.getItem("userId");
-    // 미접수 사용자
-    if (userId === null) {
-      Swal.fire("접수되지 않은 사용자입니다.", "홈페이지에서 접수하실 수 있습니다.", "warning");
-      return;
-    }
+  // const routeToZeroGame = async () => {
+  //   const userId = localStorage.getItem("userId");
+  //   // 미접수 사용자
+  //   if (userId === null) {
+  //     Swal.fire("접수되지 않은 사용자입니다.", "홈페이지에서 접수하실 수 있습니다.", "warning");
+  //     return;
+  //   }
 
-    const teamId = await getTeamId(userId);
-    // 미접수 사용자
-    if (teamId === null) {
-      Swal.fire("접수되지 않은 사용자입니다.", "홈페이지에서 접수하실 수 있습니다.", "warning");
-    }
-    // 팀 참가 대기 사용자 (제로게임 처음)
-    else if (teamId === "-") {
-      // Swal.fire("9월 23일을 기대해주세요!", "서면 송상현 광장에서 만나요~", "success");
-      Swal.fire("제로게임 참가 대상입니다!", "스텝이 팀을 만들 때까지 기다려주세요.", "success");
-    }
-    // 스텝에 의해 팀 참가 직후 버튼 클릭
-    else {
-      localStorage.setItem("teamId", teamId);
-      navigate(RoutePath.ZEROGAME);
-    }
+  //   const teamId = await getTeamId(userId);
+  //   // 미접수 사용자
+  //   if (teamId === null) {
+  //     Swal.fire("접수되지 않은 사용자입니다.", "홈페이지에서 접수하실 수 있습니다.", "warning");
+  //   }
+  //   // 팀 참가 대기 사용자 (제로게임 처음)
+  //   else if (teamId === "-") {
+  //     // Swal.fire("9월 23일을 기대해주세요!", "서면 송상현 광장에서 만나요~", "success");
+  //     Swal.fire("제로게임 참가 대상입니다!", "스텝이 팀을 만들 때까지 기다려주세요.", "success");
+  //   }
+  //   // 스텝에 의해 팀 참가 직후 버튼 클릭
+  //   else {
+  //     localStorage.setItem("teamId", teamId);
+  //     navigate(RoutePath.ZEROGAME);
+  //   }
+  // };
+
+  const handleServerBlock = () => {
+    Swal.fire("신청 기간이 아닙니다!", "2024년 TF을 기대해주세요!", "info");
   };
 
   return (
@@ -69,7 +67,7 @@ const MissionPage = () => {
         <s.ButtonWrapper>
           <s.ZeroGameButton
             src="/assets/mission/zerogame_go_button.svg"
-            onClick={routeToZeroGame}
+            onClick={handleServerBlock}
           />
         </s.ButtonWrapper>
       </s.Wrapper>
@@ -77,15 +75,15 @@ const MissionPage = () => {
   );
 };
 
-const getTeamId = async (userId) => {
-  // API: Get User Team
-  const res = await axios.get(ENV.SERVER_PROD_DOMAIN + API.GET_USER_TEAM, {
-    params: { userId },
-  });
-  if (Number(res.data.code) === 404) {
-    return null;
-  }
-  return res.data.teamId;
-};
+// const getTeamId = async (userId) => {
+//   // API: Get User Team
+//   const res = await axios.get(ENV.SERVER_PROD_DOMAIN + API.GET_USER_TEAM, {
+//     params: { userId },
+//   });
+//   if (Number(res.data.code) === 404) {
+//     return null;
+//   }
+//   return res.data.teamId;
+// };
 
 export default MissionPage;
--- client/src/pages/RuleChoose/index.jsx
+++ client/src/pages/RuleChoose/index.jsx
@@ -1,19 +1,19 @@
 import React, { useEffect } from "react";
 import * as s from "./style";
-import { Link } from "react-router-dom";
-import { RoutePath } from "@constants/enums";
 import { motion } from "framer-motion";
 
 import PageTemplate from "../PageTemplate";
 import TopNavBar from "@common/layer/TopNavBar";
+import Swal from "sweetalert2";
 
 const RuleChoosePage = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  const STEP_GOOGLE_FORM =
-    "https://docs.google.com/forms/d/e/1FAIpQLSf9hrEpXCUEBd0epFID7QqK52g_P8B6tMQBipyu9rmH1q9Idw/viewform";
+  const handleServerBlock = () => {
+    Swal.fire("신청 기간이 아닙니다!", "2024년 TF을 기대해주세요!", "info");
+  };
 
   return (
     <PageTemplate>
@@ -21,28 +21,22 @@ const RuleChoosePage = () => {
       <s.Wrapper>
         <s.Container>
           <s.ButtonSection as={motion.div} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-            <Link to={RoutePath.REGISTER}>
-              <s.ParticipantButton>
-                <s.ButtonInnerText>참가자</s.ButtonInnerText>
-              </s.ParticipantButton>
-            </Link>
+            <s.ParticipantButton onClick={handleServerBlock}>
+              <s.ButtonInnerText>참가자</s.ButtonInnerText>
+            </s.ParticipantButton>
             <s.Description>0923 행사를 즐기는 참가자로 신청할래요!</s.Description>
           </s.ButtonSection>
           <s.BR />
           <s.ButtonSection as={motion.div} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-            <Link to={STEP_GOOGLE_FORM}>
-              <s.StepButton>
-                <s.StepButtonInnerText>스텝</s.StepButtonInnerText>
-              </s.StepButton>
-            </Link>
+            <s.StepButton onClick={handleServerBlock}>
+              <s.StepButtonInnerText>스텝</s.StepButtonInnerText>
+            </s.StepButton>
             <s.Description>준비 및 행사를 돕는 스텝으로 신청할래요!</s.Description>
           </s.ButtonSection>
           <s.BR />
           <s.ReQRWrapper as={motion.div} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
             <s.ReQRLable>이미 참가자로 접수하였다면?</s.ReQRLable>
-            <Link to={RoutePath.RECONFIRM_QR}>
-              <s.ReQRLinker>내 QR 코드 보러가기</s.ReQRLinker>
-            </Link>
+            <s.ReQRLinker onClick={handleServerBlock}>내 QR 코드 보러가기</s.ReQRLinker>
           </s.ReQRWrapper>
         </s.Container>
       </s.Wrapper>
